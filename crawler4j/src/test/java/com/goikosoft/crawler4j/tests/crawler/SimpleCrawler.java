82661839db6ff54b2bfed01e1c2ad131a55398f1
dgoiko <12698425+dgoiko@users.noreply.github.com>
2020-05-11 02:04:22 +0200

    Manual test for crawler
    To manually execute a crawler and test

---
+++ crawler4j/src/test/java/com/goikosoft/crawler4j/tests/crawler/SimpleCrawler.java
@@ -0,0 +1,45 @@
+package com.goikosoft.crawler4j.tests.crawler;
+
+import com.goikosoft.crawler4j.crawler.CrawlController;
+import com.goikosoft.crawler4j.crawler.WebCrawler;
+import com.goikosoft.crawler4j.robotstxt.RobotstxtConfig;
+import com.goikosoft.crawler4j.robotstxt.RobotstxtServer;
+import com.goikosoft.crawler4j.selenium.PageFetcherSelenium;
+import com.goikosoft.crawler4j.selenium.ParserSelenium;
+import com.goikosoft.crawler4j.selenium.SeleniumCrawlConfig;
+import com.goikosoft.crawler4j.url.TLDList;
+
+public class SimpleCrawler {
+
+    public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "/data/crawl/root";
+        int numberOfCrawlers = 1;
+
+        SeleniumCrawlConfig config = new SeleniumCrawlConfig();
+        config.setThreadMonitoringDelaySeconds(1);
+        config.setCleanupDelaySeconds(1);
+        config.setThreadShutdownDelaySeconds(1);
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setDefaultToSelenium(true);
+        config.setCookiesSelemiun(true);
+        // Instantiate the controller for this crawl.
+        TLDList tldList = new TLDList(config);
+        ParserSelenium parser = new ParserSelenium(config, tldList);
+        PageFetcherSelenium pageFetcher = new PageFetcherSelenium(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, parser, robotstxtServer, tldList);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("https://www.google.com");
+
+        // The factory which creates instances of crawlers.
+        CrawlController.WebCrawlerFactory<WebCrawler> factory = WebCrawler::new;
+
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
+    }
+}
